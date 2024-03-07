4d7a3e585e3870af5c6f22235addb9e1cbad5ae9
lucasgianine <lucas.esilva@sptech.school>
2024-03-07 18:28:32 -0300

    Cria componente para logo

---
+++ src/components/LogoCohive/LogoCohive.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type LogoCohiveProps = {
+  color?: 'purple' | 'white' | 'black'
+}
+
+const logoColors = {
+  purple: 'logoCohive.svg',
+  white: 'logoCohiveWhite.svg',
+  black: 'logoCohiveBlack.svg'
+}
+
+function LogoCohive(props: LogoCohiveProps) {
+  const { color } = props
+
+  return (
+    <img
+    src={logoColors[color ?? 'purple']}
+    alt='Logo da Cohive'
+    />
+  )
+}
+
+export default LogoCohive
