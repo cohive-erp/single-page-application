d1706f15eea22f1beaaafa57c16f5f83d74ef3d9
lucasgianine <lucas.esilva@sptech.school>
2024-03-07 18:28:59 -0300

    Cria NavBarAnchor para navegar por seções

---
+++ src/components/NavBar/NavBarAnchor.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export type NavBarAnchorProps = {
+  section: 'inicio' | 'sobre' | 'funcionalidade' | 'contato'
+}
+
+export default function NavBarAnchor(props: NavBarAnchorProps) {
+  return (
+    <a href={`#${props.section}`}
+      className='font-poppins font-sans text-base text-gray-950 hover:text-purple-500'
+    >
+      {props.section}
+    </a>
+  )
+}
