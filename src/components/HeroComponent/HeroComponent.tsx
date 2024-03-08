0e101fceec677da814f476b69aae0acfdf964753
lucasgianine <lucas.esilva@sptech.school>
2024-03-08 01:22:38 -0300

    Cria componente de Hero

---
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Paragraph from '../Paragraph/Paragraph'
+
+function HeroSection() {
+  return (
+    <>
+      <div className='hero min-h-screen bg-base-200'>
+        <div className='hero-content text-center'>
+          <div className='max-w-md flex flex-col gap-2'>
+            <img
+              src='./heroImage.svg'
+              alt='Gerencie melhor sua empresa com o sistema de gestão da Cohive'
+              className='' />
+            <Paragraph size='h3'>
+              Organize mais melhorando o <span className='text-purple-500'>sistema de gestão</span> da sua empresa.
+            </Paragraph>
+            <div className='inline-flex gap-4 justify-center'>
+              <a href='/'>
+                <img
+                  src='./appleDownload.svg'
+                  alt='Faça o download na App Store'
+                  width={150}
+                  className='rounded-md'
+                />
+              </a>
+              <a href='/'>
+                <img
+                  src='./googleDownload.svg'
+                  alt='Faça o download na Google Store'
+                  width={150}
+                  className='rounded-md'
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default HeroSection
