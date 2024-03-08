a3e36179eb7c777a7d9d1c79517c0ade4e7e27c6
lucasgianine <lucas.esilva@sptech.school>
2024-03-08 19:17:18 -0300

    Cria stories para teste do Footer

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Footer'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/Footer'
+}
+
+export const Example = () => {
+  return (
+    <Footer />
+  )
+}
