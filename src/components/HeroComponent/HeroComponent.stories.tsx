6fd0187baed4d95d65f5c03b4205002fa0c2afe0
lucasgianine <lucas.esilva@sptech.school>
2024-03-08 01:04:08 -0300

    Cria stories para componente Hero

---
+++ src/components/HeroComponent/HeroComponent.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroComponent from './HeroComponent'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/Hero'
+}
+
+export const Example = () => {
+  return (
+    <HeroComponent />
+  )
+}
