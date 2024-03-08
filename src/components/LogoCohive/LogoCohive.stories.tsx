1b34214cb8d17a29e8cf9228b455102aca691f37
lucasgianine <lucas.esilva@sptech.school>
2024-03-07 21:35:43 -0300

    Cria stories para Logo

---
+++ src/components/LogoCohive/LogoCohive.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LogoCohive from './LogoCohive'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/Logo Cohive'
+}
+
+export const Example = () => {
+  return (
+    <LogoCohive />
+  )
+}
