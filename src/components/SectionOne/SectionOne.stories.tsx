1389a32f9fced51e674b451239fb55966e31666d
lucasgianine <lucas.esilva@sptech.school>
2024-03-27 20:56:29 -0300

    Cria stories para seção 1

---
+++ src/components/SectionOne/SectionOne.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SectionOne from './SectionOne'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/SectionOne'
+}
+
+export const Example = () => {
+  return (
+    <SectionOne />
+  )
+}
