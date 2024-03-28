484b5ba2322894e9b298090ffbc917afb1610067
lucasgianine <lucas.esilva@sptech.school>
2024-03-27 21:11:51 -0300

    Atualiza título da seção 2

--- src/components/SectionTwo/SectionTwo.tsx
+++ src/components/SectionTwo/SectionTwo.tsx
@@ -7,10 +7,14 @@ function SectionTwo() {
     <div className='flex basis-full'>
       <div className='hero min-h-screen flex flex-row justify-evenly items-center'>
         <div className='flex flex-col gap-6 basis-4/12'>
-          <Paragraph size='title'>
-            Evite complexidade.
-            Melhore o controle do seu estoque de maneira <span className='text-purple-500'>rápida</span>. <span className='text-purple-500'>prática</span>. <span className='text-purple-500'>útil</span>.
-          </Paragraph>
+          <div>
+            <Paragraph size='title'>
+              Evite complexidade.
+            </Paragraph>
+            <Paragraph size='title'>
+              Melhore o controle do seu estoque de maneira <span className='text-purple-500'>rápida</span>. <span className='text-purple-500'>prática</span>. <span className='text-purple-500'>útil</span>.
+            </Paragraph>
+          </div>
 
           <Paragraph size='content'>
             Utilize nosso sistema para ampliar seu negócio sem depender de sistemas complexos que mais te complicam do que te ajudam!
