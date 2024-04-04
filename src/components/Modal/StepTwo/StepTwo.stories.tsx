096b4dd441caafa3c870f4002827c022360c92db
khauanegs <khauane.souza@sptech.school>
2024-04-04 20:47:55 -0300

    Criando stories

---
+++ src/components/Modal/StepTwo/StepTwo.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import StepTwo from './StepTwo'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/StepTwo'
+}
+
+export const Example = () => {
+  return (
+    <StepTwo />
+  )
+}
