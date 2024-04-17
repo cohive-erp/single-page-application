0ce6e565c07f075bd678419256704b59e0c4da82
khauanegs <khauane.souza@sptech.school>
2024-04-17 16:49:08 -0300

    criando stories

---
+++ src/components/Modal/CadastroProduto/CadastroProduto.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CadastroProduto from './CadastroProduto'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/CadastroProduto'
+}
+
+export const Example = () => {
+  return (
+    <CadastroProduto />
+  )
+}
