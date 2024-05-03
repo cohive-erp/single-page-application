31f7320d231288f60c3cdbdbccb2bed8298f2bd5
lucasgianine <lucas.esilva@sptech.school>
2024-05-02 23:32:48 -0300

    Altera variáveis, adiciona slice e refatora lint

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 
-type AvatarProps = {initial: string}
+type AvatarProps = {
+    name: string
+}
 
 function Avatar(props: AvatarProps) {
-    const {initial} = props;
+    const { name } = props
+
+    const initial = name.slice(0, 1)
     return (
-        <div className="dropdown dropdown-bottom">
-            <div className="avatar placeholder" tabIndex={0} role='button'>
-                <div className="bg-neutral text-neutral-content rounded-full w-16">
-                    <span className="text-xl">{initial.toUpperCase()}</span>
+        <div className='dropdown dropdown-bottom'>
+            <div className='avatar placeholder' tabIndex={0} role='button'>
+                <div className='bg-neutral text-neutral-content rounded-full w-16'>
+                    <span className='text-2xl'>{initial.toUpperCase()}</span>
                 </div>
             </div>
-            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Abrir Perfil</a></li>
-                <li><a>Logout</a></li>
+            <ul tabIndex={0} className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52'>
+                <li><button>Abrir Perfil</button></li>
+                <li><button>Logout</button></li>
             </ul>
         </div>
-
     )
 }
 
