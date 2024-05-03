4a821a8e3a81e0ec48d2c8c1ebeff9a79525e43f
lucasgianine <lucas.esilva@sptech.school>
2024-05-02 23:40:17 -0300

    Altera Avatar de pasta, adiciona ícones no dropdown e muda posição do dropdown

--- src/components/Header/components/Avatar/Avatar.tsx
+++ src/components/Header/components/Avatar/Avatar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import PersonIcon from '@mui/icons-material/Person'
+import LogoutIcon from '@mui/icons-material/Logout'
 
 type AvatarProps = {
     name: string
@@ -9,15 +11,25 @@ function Avatar(props: AvatarProps) {
 
     const initial = name.slice(0, 1)
     return (
-        <div className='dropdown dropdown-bottom'>
+        <div className='dropdown dropdown-bottom dropdown-end'>
             <div className='avatar placeholder' tabIndex={0} role='button'>
                 <div className='bg-neutral text-neutral-content rounded-full w-16'>
                     <span className='text-2xl'>{initial.toUpperCase()}</span>
                 </div>
             </div>
             <ul tabIndex={0} className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52'>
-                <li><button>Abrir Perfil</button></li>
-                <li><button>Logout</button></li>
+                <li>
+                    <button>
+                        <PersonIcon fontSize='small' />
+                        Abrir Perfil
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <LogoutIcon fontSize='small' />
+                        Logout
+                    </button>
+                </li>
             </ul>
         </div>
     )
