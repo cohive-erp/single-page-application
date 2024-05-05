578f4c2dc6cff4a315ce5eeb8e91e1b2a1542e7c
lucasgianine <lucas.esilva@sptech.school>
2024-05-05 18:56:28 -0300

    Atualiza stories e imagem

--- src/components/BarChart/BarChart.stories.tsx
+++ src/components/BarChart/BarChart.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BarChartComponent from './BarChart'
+import BarChartSkeleton from './BarChartSkeleton'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -9,62 +10,62 @@ export default {
 const data = [
   {
     month: 'Jan',
-    vendido: 6856,
+    Vendido: 6856,
     'Em estoque': 2856,
   },
   {
     month: 'Feb',
-    vendido: 7032,
+    Vendido: 7032,
     'Em estoque': 3032,
   },
   {
     month: 'Mar',
-    vendido: 8023,
+    Vendido: 8023,
     'Em estoque': 7033,
   },
   {
     month: 'Apr',
-    vendido: 5512,
+    Vendido: 5512,
     'Em estoque': 4512,
   },
   {
     month: 'May',
-    vendido: 5600,
+    Vendido: 5600,
     'Em estoque': 8060,
   },
   {
     month: 'Jun',
-    vendido: 1000,
+    Vendido: 1000,
     'Em estoque': 8539,
   },
   {
     month: 'Jul',
-    vendido: 8590,
+    Vendido: 8590,
     'Em estoque': 5340,
   },
   {
     month: 'Aug',
-    vendido: 8010,
+    Vendido: 8010,
     'Em estoque': 7010,
   },
   {
     month: 'Sep',
-    vendido: 7590,
+    Vendido: 7590,
     'Em estoque': 6950,
   },
   {
     month: 'Oct',
-    vendido: 7180,
+    Vendido: 7180,
     'Em estoque': 3345,
   },
   {
     month: 'Nov',
-    vendido: 1210,
+    Vendido: 1210,
     'Em estoque': 1330,
   },
   {
     month: 'Dec',
-    vendido: 6013,
+    Vendido: 6013,
     'Em estoque': 5321,
   },
 ]
@@ -73,7 +74,20 @@ export const Example = () => {
   return (
     <BarChartComponent
       data={data}
-      section='Produtos'
-      variant={['Quant. Vendida', 'Quant. em Estoque']} />
+      section='Produtos' />
+  )
+}
+
+export const NoData = () => {
+  return (
+    <BarChartComponent
+      data={[]}
+      section='Produtos' />
+  )
+}
+
+export const Loading = () => {
+  return (
+    <BarChartSkeleton section='Produtos' />
   )
 }
