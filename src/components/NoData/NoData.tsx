1e725e007bb0df9f7363a398912b55a6dedefbd7
lucasgianine <lucas.esilva@sptech.school>
2024-05-05 18:55:16 -0300

    Componente caso não haja data

---
+++ src/components/NoData/NoData.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Paragraph from '../Paragraph/Paragraph'
+
+function NoData() {
+  return (
+    <div className='flex flex-col w-full h-[50%] justify-center items-center'>
+      <img src='./noData.svg' alt='Dados não encontrado' width={40} height={40} />
+      <Paragraph size='h3-regular'>Não há dados suficientes.</Paragraph>
+    </div>
+  )
+}
+
+export default NoData
