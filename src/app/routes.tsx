36ab6fd19a83418f09c1f929b6df6f622f996c68
lucasgianine <lucas.esilva@sptech.school>
2024-05-08 18:10:46 -0300

    Adiciona Login nas rotas

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home/Home.tsx'
+import LoginPage from '../pages/Login/Login.tsx'
 
 function Rotas() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
