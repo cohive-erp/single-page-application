43a2b22867ef939a81746c6fd5989f36a59e0d92
khauanegs <khauane.souza@sptech.school>
2024-05-08 00:18:48 -0300

    segunda parte do cadastro

---
+++ src/components/CadastroTwo/CadastroTwo.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CadastroTwo from './CadastroTwo'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/CadastroTwo'
+}
+
+export const Example = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <CadastroTwo />
+    </div>
+  )
+}
