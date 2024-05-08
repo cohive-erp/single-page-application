f509cf2910fd3faa0a942f3cb9a6d0519b1a4f8d
khauanegs <khauane.souza@sptech.school>
2024-05-08 00:18:09 -0300

    primeira parte do cadastro

---
+++ src/components/CadastroOne/CadastroOne.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CadastroOne from './CadastroOne'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/CadastroOne'
+}
+
+export const Example = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <CadastroOne />
+    </div>
+  )
+}
