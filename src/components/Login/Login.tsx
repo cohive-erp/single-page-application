f3c56cf49062487126853a5b0f90000b0fa14062
lucasgianine <lucas.esilva@sptech.school>
2024-05-09 10:39:44 -0300

    Conecta Login com a função authenticateUser

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MailOutlineIcon from '@mui/icons-material/MailOutline'
 import KeyIcon from '@mui/icons-material/Key'
 import Paragraph from '../Paragraph/Paragraph.tsx'
 import Button from '../Button/Button.tsx'
+import { AuthUserCommand } from '../../lib/types/AuthUserCommand.ts'
+import authenticateUser from '../../lib/services/authenticateUser.ts'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState<string>('')
+  const [senha, setSenha] = useState<string>('')
+
+  const handleInputChange = (event, setStateFunction) => {
+    setStateFunction(event.target.value)
+  }
+
+  const handleAuthenticate = async () => {
+    const data: AuthUserCommand = { email, senha }
+
+    await authenticateUser(data)
+      .then(() => {
+        navigate('/dashboard')
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
   return (
     <div className='flex flex-col font-poppins justify-between gap-8'>
       <div className='flex flex-col justify-center items-center gap-2'>
@@ -13,22 +36,22 @@ function Login() {
       </div>
       <label className='input bg-white border-black input-bordered text-[#9A9696] flex items-center gap-2'>
         <MailOutlineIcon fontSize='small' />
-        <input type='email' className='grow text-black' placeholder='Email' />
+        <input type='email' value={email} onChange={(e) => handleInputChange(e, setEmail)} className='grow text-black' placeholder='Email' />
       </label>
       <label className='input bg-white border-black input-bordered text-[#9A9696] flex items-center gap-2'>
         <KeyIcon fontSize='small' />
-        <input type='password' className='grow text-black' placeholder='Senha' />
+        <input type='password' value={senha} onChange={(e) => handleInputChange(e, setSenha)} className='grow text-black' placeholder='Senha' />
       </label>
       <div className='flex flex-col gap-2'>
         <div className='flex flex-col items-center'>
-          <Button content='Entrar' className='w-full' onClick={() => console.log()} />
+          <Button content='Entrar' className='w-full' onClick={handleAuthenticate} />
           <Paragraph size='h6'>ou</Paragraph>
-          <Button content='Entrar com o Google' color='white' className='w-full' onClick={() => console.log()} />
+          <Button content='Entrar com o Google' color='white' className='w-full' onClick={handleAuthenticate} />
         </div>
 
         <div className='flex flex-row justify-between'>
-        <Paragraph size='h6'><a className='text-purple-500 underline' href='/signup'>Ainda não tenho conta</a></Paragraph>
-        <Paragraph size='h6'><a className='text-purple-500 underline' href='/redefinir-senha'>Esqueci minha senha</a></Paragraph>
+          <Paragraph size='h6'><a className='text-purple-500 underline' href='/signup'>Ainda não tenho conta</a></Paragraph>
+          <Paragraph size='h6'><a className='text-purple-500 underline' href='/redefinir-senha'>Esqueci minha senha</a></Paragraph>
         </div>
       </div>
     </div>
