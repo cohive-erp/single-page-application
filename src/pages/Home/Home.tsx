e13d7e93c6bc8cdd39f8ac20ed39fb3d22b533ee
lucasgianine <lucas.esilva@sptech.school>
2024-05-14 10:18:21 -0300

    Aplica Helmet para metadados nas páginas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,10 +5,14 @@ import HeroComponent from '../../components/HeroComponent/HeroComponent.tsx'
 import SectionOne from '../../components/SectionOne/SectionOne.tsx'
 import SectionTwo from '../../components/SectionTwo/SectionTwo.tsx'
 import SectionFive from '../../components/SectionFive/SectionFive.tsx'
+import { Helmet } from 'react-helmet'
 
 function Home() {
   return (
     <div className='bg-slate-200'>
+      <Helmet>
+        <title>Início | Cohive</title>
+      </Helmet>
       <NavBar />
       <HeroComponent />
       <SectionOne />
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import NavBar from '../../components/NavBar/NavBar.tsx'
 import Footer from '../../components/Footer/Footer.tsx'
 import Login from '../../components/Login/Login.tsx'
+import { Helmet } from 'react-helmet'
 
 function LoginPage() {
   return (
     <div className='bg-slate-200 flex flex-col justify-center items-center'>
+      <Helmet>
+        <title>Login | Cohive</title>
+      </Helmet>
       <NavBar />
       <div className='w-[50%] h-full flex justify-center items-center pt-12 p-32'>
         <Login />
