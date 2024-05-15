3d258fa5d2b892c72d0d881462c5e59cba540606
lucasgianine <lucas.esilva@sptech.school>
2024-05-15 18:47:55 -0300

    Cria seção 4

---
+++ src/components/SectionFour/SectionFour.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SectionFour from './SectionFour'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/SectionFour'
+}
+
+export const Example = () => {
+  return (
+    <SectionFour />
+  )
+}
