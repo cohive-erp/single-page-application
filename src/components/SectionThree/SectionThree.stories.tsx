87c099f02dcc8d99e6f7f5a84dc3b216102e8977
lucasgianine <lucas.esilva@sptech.school>
2024-05-15 18:47:35 -0300

    Cria seção 3

---
+++ src/components/SectionThree/SectionThree.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SectionThree from './SectionThree'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/SectionThree'
+}
+
+export const Example = () => {
+  return (
+    <SectionThree />
+  )
+}
