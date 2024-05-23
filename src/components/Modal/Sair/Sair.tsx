35d6dd741fea257b2976a02d2b6db93e1c4de189
khauanegs <khauane.souza@sptech.school>
2024-05-23 17:13:17 -0300

    Cria modal para deslogar

---
+++ src/components/Modal/Sair/Sair.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from '../../Button/Button'
+
+function ApagarProduto() {
+    return (
+        <>
+            <button className='btn' onClick={() => document.getElementById('my_modal_1')?.showModal()}>open modal</button>
+            <dialog id='my_modal_1' className='modal'>
+                <div className='modal-box w-[30%] h-[25%] flex flex-col justify-center items-center'>
+                    <h3 className='font-bold text-lg'>Deseja mesmo sair?</h3>
+                    <div className='modal-action w-full flex justify-cente'>
+                        <form method='dialog' className='w-full'>
+                            <div className='card-actions flex justify-between w-full px-4'>
+                                <Button content='Sair' className='w-[45%] shadow-sm' onClick={() => console.log()} />
+                                <Button content='Cancelar' className='w-[45%] shadow-sm' color='secondary' onClick={() => console.log()} />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </>
+    )
+}
+
+export default ApagarProduto
