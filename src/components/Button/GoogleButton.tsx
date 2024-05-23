da6492d0f3622dfd9cd4475c4db29dbf369a1a6a
lucasgianine <lucas.esilva@sptech.school>
2024-05-23 17:15:02 -0300

    Cria botão de login por Google

---
+++ src/components/Button/GoogleButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Paragraph from '../Paragraph/Paragraph.tsx'
+
+function GoogleButton(props: { isLogin?: boolean }) {
+  const { isLogin } = props
+
+  return (
+    <button
+      className='w-full h-[52px] rounded-lg border-2 border-gray-300 shadow-sm bg-white flex justify-center gap-4 items-center hover:bg-gray-100 transition'
+    >
+      <img
+        src='/googleIcon.png'
+        alt='Fazer login com o Google'
+        title='Fazer login com o Google'
+        referrerPolicy='no-referrer'
+        width={24}
+      />
+
+      <Paragraph size='h4'>
+        {isLogin ? 'Login com Google' : 'Cadastrar com Google'}
+      </Paragraph>
+    </button>
+  )
+}
+
+export default GoogleButton
