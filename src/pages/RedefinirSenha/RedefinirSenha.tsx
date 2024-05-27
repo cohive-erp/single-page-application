2b96a0534b14ad738f260f4a901ab49fbb7ad17c
lucasgianine <lucas.esilva@sptech.school>
2024-05-27 18:14:47 -0300

    Adiciona título na página de redefinir senha

--- src/pages/RedefinirSenha/RedefinirSenha.tsx
+++ src/pages/RedefinirSenha/RedefinirSenha.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import NavBar from '../../components/NavBar/NavBar.tsx'
 import Footer from '../../components/Footer/Footer.tsx'
 import RedefinirSenha from '../../components/RedefinirSenha/RedefinirSenha.tsx'
+import { Helmet } from 'react-helmet'
 
 function RedefinirSenhaPage() {
   return (
     <div className='bg-slate-200 flex flex-col justify-center items-center'>
+      <Helmet>
+        <title>Redefinir senha | Cohive</title>
+      </Helmet>
       <NavBar />
       <div className='w-[50%] h-full flex justify-center items-center pr-32 pl-32 pt-44 pb-44'>
         <RedefinirSenha />
