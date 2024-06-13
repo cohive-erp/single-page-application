4d0e56563859fa063244d8a9cb556273cf6427ed
lucasgianine <lucas.esilva@sptech.school>
2024-06-13 08:11:20 -0300

    Cria e exporta página de Dashboard e Estoque

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,22 +1,120 @@
-import React from 'react'
-import NavBar from '../../components/NavBar/NavBar.tsx'
+import React, { useState } from 'react'
 import Footer from '../../components/Footer/Footer.tsx'
-import RedefinirSenha from '../../components/RedefinirSenha/RedefinirSenha.tsx'
+import ProductTable from '../../components/Table/ProductTable.tsx'
 import { Helmet } from 'react-helmet'
+import Header from '../../components/Header/Header.tsx'
+import Paragraph from '../../components/Paragraph/Paragraph.tsx'
+import Kpi from '../../components/Kpi/Kpi.tsx'
+import BarChartComponent from '../../components/BarChart/BarChart.tsx'
+import Button from '../../components/Button/Button.tsx'
 
-function DashboardPage() {
+const data = [
+  {
+    month: 'Jan',
+    Vendido: 6856,
+    'Em estoque': 2856,
+  },
+  {
+    month: 'Feb',
+    Vendido: 7032,
+    'Em estoque': 3032,
+  },
+  {
+    month: 'Mar',
+    Vendido: 8023,
+    'Em estoque': 7033,
+  },
+  {
+    month: 'Apr',
+    Vendido: 5512,
+    'Em estoque': 4512,
+  },
+  {
+    month: 'May',
+    Vendido: 5600,
+    'Em estoque': 8060,
+  },
+  {
+    month: 'Jun',
+    Vendido: 1000,
+    'Em estoque': 8539,
+  },
+  {
+    month: 'Jul',
+    Vendido: 8590,
+    'Em estoque': 5340,
+  },
+  {
+    month: 'Aug',
+    Vendido: 8010,
+    'Em estoque': 7010,
+  },
+  {
+    month: 'Sep',
+    Vendido: 7590,
+    'Em estoque': 6950,
+  },
+  {
+    month: 'Oct',
+    Vendido: 7180,
+    'Em estoque': 3345,
+  },
+  {
+    month: 'Nov',
+    Vendido: 1210,
+    'Em estoque': 1330,
+  },
+  {
+    month: 'Dec',
+    Vendido: 6013,
+    'Em estoque': 5321,
+  },
+]
+
+function EstoquePage() {
+  const [loja, setLoja] = useState()
+
   return (
     <div className='bg-slate-200 flex flex-col justify-center items-center'>
       <Helmet>
-        <title>Painel de {sessionStorage.getItem('nome')} | Sistema ERP: Gestão rápida, prática e útil | Cohive</title>
+        <title>Painel de Raquel | Sistema ERP: Gestão rápida, prática e útil | Cohive</title>
       </Helmet>
-      <NavBar />
-      <div className='w-[50%] h-full flex justify-center items-center pr-32 pl-32 pt-[8%] pb-[8%]'>
-        <RedefinirSenha />
+      <Header name='Raquel' totalProducts={2} />
+      <div className='w-[90%] h-[80%] flex flex-col gap-4 justify-center items-center pt-[2%] pb-[2%]'>
+          <div className='bg-slate-100 w-full rounded-xl shadow-md h-14 flex justify-center items-center p-4'>
+            <Paragraph size='h2'>Visão Geral da Loja</Paragraph>
+          </div>
+
+          <div className='flex flex-row gap-4 justify-between w-full'>
+            <div className='flex flex-col gap-4 w-[70%] h-[10%]'>
+              <div className='w-full flex flex-row gap-4 justify-between items-center'>
+                <Kpi value='teste' title='teste' />
+                <Kpi value='teste' title='teste' />
+                <Kpi value='R$5000' title='teste' />
+              </div>
+              <div className='flex h-[60%]'>
+              <BarChartComponent data={data} />
+              </div>
+            </div>
+
+            <div className='card w-full h-full bg-slate-100 shadow-xl'>
+              <div className='card-body h-full'>
+                <Paragraph size='h2'>🚨 Ocorrências</Paragraph>
+
+                <div className='card-actions justify-center'>
+                    <Button content='Gerar relatório' className='w-full shadow-sm' onClick={() => {}} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='w-full'>
+            <BarChartComponent data={data} />
+          </div>
       </div>
       <Footer />
     </div>
   )
 }
 
-export default DashboardPage
+export default EstoquePage
--- src/pages/Estoque/Estoque.tsx
+++ src/pages/Estoque/Estoque.tsx
@@ -1,21 +1,97 @@
-import React from 'react'
-import NavBar from '../../components/NavBar/NavBar.tsx'
+import React, { useEffect, useState } from 'react'
 import Footer from '../../components/Footer/Footer.tsx'
-import RedefinirSenha from '../../components/RedefinirSenha/RedefinirSenha.tsx'
+import ProductTable from '../../components/Table/ProductTable.tsx'
 import { Helmet } from 'react-helmet'
+import Header from '../../components/Header/Header.tsx'
+import NovoProduto from '../../components/Modal/NovoProduto/NovoProduto.tsx'
+import EditarProduto from '../../components/Modal/EditarProduto/EditarProduto.tsx'
+import Relatorio from '../../components/Modal/Relatorio/Relatorio.tsx'
+import getEstoque from '../../lib/services/Get/get-estoque.ts'
+import { ProdutoResult } from '../../lib/types/ProdutoResult.ts'
 
 function EstoquePage() {
+  const [openNovoProduto, setOpenNovoProduto] = useState(false)
+  const [openRelatorio, setOpenRelatorio] = useState(false)
+  const [openEditarProduto, setOpenEditarProduto] = useState(false)
+  const [openDeletar, setOpenDeletar] = useState(false)
+  const [produtos, setProdutos] = useState<ProdutoResult[]>([])
+
+  const token = sessionStorage.getItem('sessionToken')
+  const nome = JSON.parse(sessionStorage.getItem('valores') ?? '{}').nome
+
+  const handleOpenNovoProduto = () => {
+    setOpenNovoProduto(!openNovoProduto)
+  }
+
+  const handleOpenRelatorio = () => {
+    setOpenRelatorio(!openRelatorio)
+  }
+
+  const handleOpenEditarProduto = () => {
+    setOpenEditarProduto(!openEditarProduto)
+  }
+
+  const handleOpenDeletar = () => {
+    setOpenDeletar(!openDeletar)
+  }
+
+  const getAllProducts = async () => {
+    await getEstoque(token)
+      .then((response) => {
+        setProdutos(response)
+      })
+      .catch((e) => {
+        console.error('Erro ao tentar obter produtos', e)
+      })
+  }
+
+  useEffect(() => {
+    getAllProducts()
+  }, [])
+
   return (
-    <div className='bg-slate-200 flex flex-col justify-center items-center'>
-      <Helmet>
-        <title>Estoque: ({sessionStorage.getItem('nome_loja')}) | Sistema ERP: Gestão rápida, prática e útil | Cohive</title>
-      </Helmet>
-      <NavBar />
-      <div className='w-[50%] h-full flex justify-center items-center pr-32 pl-32 pt-[8%] pb-[8%]'>
-        <RedefinirSenha />
+    <>
+      <div className='bg-slate-200 flex flex-col justify-center items-center'>
+        <Helmet>
+          <title>Estoque: {nome ?? ''} | Sistema ERP: Gestão rápida, prática e útil | Cohive</title>
+        </Helmet>
+        <Header name='Raquel' totalProducts={2} />
+        <div className='w-full h-[80%] flex justify-center items-center pt-[2%] pb-[2%]'>
+          <ProductTable
+            tableResult={produtos}
+            handleOpenNovoProduto={handleOpenNovoProduto}
+            handleOpenRelatorio={handleOpenRelatorio}
+            handleOpenEditarProduto={handleOpenEditarProduto}
+            handleOpenDeletar={handleOpenDeletar}
+          />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+
+      {openNovoProduto && (
+        <div className='fixed w-full h-full top-[50%] left-[50%] z-50 bg-black bg-opacity-20' style={{ transform: 'translate(-50%, -50%)' }}>
+          <div className='fixed top-[50%] left-[50%]' style={{ transform: 'translate(-50%, -50%)' }}>
+            <NovoProduto handleOpenNovoProduto={handleOpenNovoProduto} />
+          </div>
+        </div>
+      )}
+
+      {openEditarProduto && (
+        <div className='fixed w-full h-full top-[50%] left-[50%] z-50 bg-black bg-opacity-20' style={{ transform: 'translate(-50%, -50%)' }}>
+          <div className='fixed top-[50%] left-[50%]' style={{ transform: 'translate(-50%, -50%)' }}>
+            <EditarProduto id={0} handleOpenEditarProduto={handleOpenEditarProduto} />
+          </div>
+        </div>
+      )}
+
+      {openDeletar && (
+        <div className='fixed w-full h-full top-[50%] left-[50%] z-50 bg-black bg-opacity-20' style={{ transform: 'translate(-50%, -50%)' }}>
+          <div className='fixed top-[50%] left-[50%]' style={{ transform: 'translate(-50%, -50%)' }}>
+            {/* <NovoProduto id={0} /> */}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
