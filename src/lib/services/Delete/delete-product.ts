4a9c366c60b4eeb710da71289b658a28e4a9354d
lucasgianine <lucas.esilva@sptech.school>
2024-06-19 18:02:58 -0300

    Atualiza delete

--- src/lib/services/Delete/delete-product.ts
+++ src/lib/services/Delete/delete-product.ts
@@ -1,7 +1,7 @@
 import api from '../../client/client.ts'
 
 export default async function deleteProduct(id: number, token: string | null) {
-  return (await api.put(`/estoque/${id}`, {
+  return (await api.delete(`/estoque/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`
     }
