3bf0a2fba90e29f8f731a198b780cc903b5236ab
lucasgianine <lucas.esilva@sptech.school>
2024-10-07 21:11:21 -0300

    Change url client

--- src/lib/client/client.ts
+++ src/lib/client/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://backend-cohive.azurewebsites.net/',
+  baseURL: process.env.BACKEND_URL ?? 'http://localhost:8080'
 })
 
 export default api
